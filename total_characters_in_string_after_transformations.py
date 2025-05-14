81b36bdc8a3fd98d5e74bcba501a3ef40dcd916d
vika <yu-ling.hsu@intel.com>
2025-05-14 20:37:33 +0800

    3337

--- total_characters_in_string_after_transformations.py
+++ total_characters_in_string_after_transformations.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
         cnt = [[0, 0] for _ in range(t + 1)]
@@ -38,6 +40,55 @@ class Solution:
             # print(tmp, ret)
         return ret
     
+    def lengthAfterTransformations_1(self, s: str, t: int, nums: List[int]) -> int:
+        import string 
+    
+
+        M = [[0] * 26 for _ in range(26)]
+        x = [0] * 26 
+        modulo = 10 ** 9 + 7
+        for c in s: 
+            idx = ord(c) - ord('a')
+            x[idx] += 1 
+        for c in string.ascii_lowercase:
+            idx = ord(c) - ord('a')
+            for i in range(idx + 1, idx + nums[idx] + 1):
+                M[i % 26][idx] = 1
+        
+        def mul(M, x): 
+            return [sum([a * b % modulo for a, b in zip(r, x)]) for r in M]
+        
+        def emul(M, n): 
+            def mmul(m1, m2): 
+                ret = []
+                k = len(m2)
+                for r in m1: 
+                    ret.append([])
+                    for c in zip(*m2): 
+                        e = sum([a * b % modulo for a, b in zip(r, c)]) % modulo
+                        ret[-1].append(e)
+                return ret 
+            
+            d = len(M)
+            ret = [[0] * d for _ in range(d)]
+            for i in range(d): 
+                ret[i][i] = 1 
+            
+            while n > 0: 
+                if n & 1: 
+                    ret = mmul(M, ret)
+                M = mmul(M, M)
+                n //= 2
+            return ret
+
+        # for _ in range(t): 
+        #     x = mul(M, x)
+
+        M = emul(M, t)
+        x = mul(M, x)
+
+        return sum(x) % modulo
+
 if __name__ == "__main__": 
     s = "abcyy"
     t = 2
@@ -50,5 +101,13 @@ if __name__ == "__main__":
 
     # s = "jqktcurgdvlibczdsvnsg" 
     # t = 7517
-    ret = Solution().lengthAfterTransformations(s, t)
+
+    s = "abcyy"
+    t = 2
+    nums = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]
+
+    # s = "azbk"
+    # t = 1
+    # nums = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+    ret = Solution().lengthAfterTransformations_1(s, t, nums)
     print(ret)
